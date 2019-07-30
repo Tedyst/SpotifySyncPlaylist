6558798e6724c585423bdc5737c1cec8de572dd0
Stoica <stoicatedy@gmail.com>
2019-07-30 07:25:23 +0300

    cleanup

--- main.py
+++ main.py
@@ -1,35 +1,35 @@
 #!/usr/bin/python3
 import spotipy
-import spotipy.util as util
 import sys
-from spotifyconfig import *
-from spotifysyncutils import *
+import logging as log
+from .utils import initSpotifyAPI, get_playlist_tracks, clear_playlist
 
-scope = 'user-library-read playlist-modify-public playlist-modify-private'
-print(sys.argv)
-if len(sys.argv) is 3:
-    usernametocopy = sys.argv[1].split(':')[2]
-    playlisttocopy = sys.argv[1].split(':')[4]
-    usernametarget = sys.argv[2].split(':')[2]
-    playlisttarget = sys.argv[2].split(':')[4]
-elif len(sys.argv) < 5:
-    print("Not enough arguments.")
-    exit(0)
-else:
-    usernametocopy = sys.argv[1]
-    playlisttocopy = sys.argv[2]
-    usernametarget = sys.argv[3]
-    playlisttarget = sys.argv[4]
-token = util.prompt_for_user_token(usernametocopy, scope, client_id=CLIENT_ID,
-                                   client_secret=CLIENT_SECRET, redirect_uri='https://stoicatedy.ovh')
+if __name__ == "__main__":
+    if len(sys.argv) is 3:
+        usernametocopy = sys.argv[1].split(':')[2]
+        playlisttocopy = sys.argv[1].split(':')[4]
+        usernametarget = sys.argv[2].split(':')[2]
+        playlisttarget = sys.argv[2].split(':')[4]
+    elif len(sys.argv) < 5:
+        usernametocopy = input("Enter your user ID: ")
+        playlisttocopy = input(
+            "Enter the ID of the playlist that you want to copy: ")
+        usernametarget = input(
+            "Enter the user ID of the owner of the playlist that you want to copy to: ")
+        playlisttarget = input(
+            "Enter the ID of the playlist that you want to copy to: ")
+    else:
+        usernametocopy = sys.argv[1]
+        playlisttocopy = sys.argv[2]
+        usernametarget = sys.argv[3]
+        playlisttarget = sys.argv[4]
 
-if token:
-    sp = spotipy.Spotify(auth=token)
+    sp = initSpotifyAPI(usernametocopy)
     playlist = get_playlist_tracks(sp, usernametocopy, playlisttocopy)
-    sync_playlist(sp, usernametarget, playlisttarget, [])
+    # This sets the playlist to nothing
+    clear_playlist(sp, usernametarget, playlisttarget)
+    # This adds the song in order to the target playlist
     while len(playlist):
         sp.user_playlist_add_tracks(
             usernametarget, playlisttarget, playlist[0:99])
         playlist = playlist[100:]
-else:
-    print("No token given.")
---
+++ utils.py
@@ -0,0 +1,29 @@
+from .config import *
+import spotipy.util as util
+import spotipy
+
+
+def get_playlist_tracks(client, username, uri):
+    results = client.user_playlist_tracks(username, playlist_id=uri)
+    tracks = []
+    for i in results['items']:
+        tracks.append(i['track']['id'])
+    while results['next']:
+        results = client.next(results)
+        for i in results['items']:
+            tracks.append(i['track']['id'])
+    return tracks
+
+
+def clear_playlist(client, username, target):
+    client.user_playlist_replace_tracks(username, target, [])
+
+
+def initSpotifyAPI(username, scope="suser-library-read playlist-modify-public playlist-modify-private", client_id=CLIENT_ID,
+                   client_secret=CLIENT_SECRET, redirect_uri='https://stoicatedy.ovh'):
+    token = util.prompt_for_user_token(username, scope, client_id,
+                                       client_secret, redirect_uri='https://stoicatedy.ovh')
+    if token:
+        return spotipy.Spotify(auth=token)
+    else:
+        raise Exception("No token provided.")
